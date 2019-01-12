7c897cfdc08291d1e009e11dd89f90a268248191
Jur van Oerle <j.h.van.oerle@hva.nl>
2019-01-12 13:27:32 +0100

    Create template phaser project.

--- backend/Listener.js
+++ backend/Listener.js
@@ -1,15 +1,25 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var server = require('http').Server(app);
+var path = require('path');
 var io = require('socket.io')({
     transports: ['websocket'],
     listeners: []
 });
+
+app.use(express.static('./'));
+//app.use(express.static('assets'));
 
 //listen to Unity events on port 4567
 io.attach(4567);
 
 //Listen to server events on port 4568
 server.listen(4568);
+
+app.get('/', function(req, res)
+{
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
 
 app.get('/join_game', function (req, res)
 {
