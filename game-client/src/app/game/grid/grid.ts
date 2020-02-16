a7e873ae28b9cba9450ba5e4bc64e13a3e700773
Jur van Oerle <j.h.van.oerle@hva.nl>
2020-02-16 12:16:35 +0100

    Render outline of selected hexagons, rather than all hexagons separately

--- game-client/src/app/game/grid/grid.ts
+++ game-client/src/app/game/grid/grid.ts
@@ -1,5 +1,4 @@
 import { MapReader, TileProperty, Object } from './map-reader';
-import { Cell } from './grid';
 import { Vector } from 'vector2d/src/Vec2D';
 import { defineGrid, GridFactory, Hex, Point, Grid } from 'honeycomb-grid';
 import { Graphics, Sprite } from 'pixi.js';
@@ -126,63 +125,43 @@ export class GridManager
         });
     }
 
-    private renderSelectionCorners(corners: Point[]): void
+    private renderSelection(selection: Hex<Cell>[]): void
     {
-        const length = corners.length;
-        for (let i = 0; i < length; i++)
+        const corners: Point[] = this.getEdgeCorners(selection);
+        for (let i = 0; i < corners.length; i+= 2)
         {
-            let left: number = i - 1;
-            if (left < 0) left = length - 1;
-            const right: number = (i + 1) % length;
+            const corner1 = corners[i];
+            const corner2 = corners[i + 1];
+            // move the "pen" to the first corner
+            this.graphics.moveTo(corner1.x, corner1.y);
 
-            const v1: Vector = new Vector(corners[i].x, corners[i].y);
-            const v2: Vector = new Vector(corners[left].x, corners[left].y);
-            const v3: Vector = new Vector(corners[right].x, corners[right].y);
-    
-            const leftCorner = v1.clone().add(v2.clone().subtract(v1).normalize().multiplyByScalar(25))
-            const rightCorner = v1.clone().add(v3.clone().subtract(v1).normalize().multiplyByScalar(25))
-
-            this.graphics.moveTo(leftCorner.x, leftCorner.y);
-            this.graphics.lineTo(v1.x, v1.y);
-            this.graphics.lineTo(rightCorner.x, rightCorner.y);
-        }
+            // draw lines to the other corners
+            this.graphics.lineTo(corner2.x, corner2.y);
+        }        
     }
 
-    private renderSelectionSolid(corners: Point[]): void
+    private getEdgeCorners(hexagons: Hex<Cell>[]): Point[]
     {
-        // separate the first from the other corners
-        const [firstCorner, ...otherCorners] = corners;
-
-        // move the "pen" to the first corner
-        this.graphics.moveTo(firstCorner.x, firstCorner.y)
-
-        // draw lines to the other corners
-        otherCorners.forEach(({ x, y }) => this.graphics.lineTo(x, y))
-
-        // finish at the first corner
-        this.graphics.lineTo(firstCorner.x + 3, firstCorner.y + 3);
-    }
-
-    public renderSelection(selection: Hex<Cell>[], renderMode: SelectionRenderMode = SelectionRenderMode.Solid): void
-    {
-        selection.forEach(hex =>
+        const edges: Point[] = [];
+        let neighbor: Hex<Cell> = null;
+        hexagons.forEach((hex) =>
         {
-            if (hex)
+            const neighbors: Hex<Cell>[] = this.grid.neighborsOf(hex);
+            for(let dir = 0; dir < neighbors.length; dir++)
             {
+                neighbor = neighbors[dir];
                 const point: Point = hex.toPoint();
                 const corners = hex.corners().map(corner => corner.add(point));
-    
-                switch (renderMode)
+               
+                if(hexagons.indexOf(neighbor) === -1)
                 {
-                    case SelectionRenderMode.Solid:
-                        this.renderSelectionSolid(corners);
-                        break;
-                    case SelectionRenderMode.Corners:
-                        this.renderSelectionCorners(corners);
-                        break;
+                    const p1 = corners[dir];
+                    const p2 = corners[(dir + 1) % 6];
+                    edges.push(p1, p2);
                 }
-            }       
+            }
         });
+        return edges;
     }
 
     public getHexAt(v: Vector): Hex<Cell>
@@ -207,11 +186,12 @@ export class GridManager
         if (hex)
         {
             this.graphics.clear();
-            this.graphics.lineStyle(10, color, 1, 0.5);
+            this.graphics.lineStyle(8, color, 1, 0.5, );
 
             const neighbors: Hex<Cell>[] = this.grid.neighborsOf(hex);
-            this.renderSelection(neighbors, SelectionRenderMode.Corners);
-            this.renderSelection([hex], SelectionRenderMode.Solid);
+            neighbors.push(hex);
+
+            this.renderSelection(neighbors);
             this.graphics.endFill();
         }
     }
